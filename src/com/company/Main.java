0bbca449ce1428f4900756093e03a97204a8ad1a
Piotr601 <61516203+Piotr601@users.noreply.github.com>
2021-06-10 19:52:36 +0200

    v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,69 +5,102 @@ import java.awt.*;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Main extends JPanel implements Runnable{
+public class Main extends JPanel implements Runnable {
 
-    Main(JFrame frame){
+
+    Main(JFrame frame, Color col) {
+        this.setLayout(null);
+        this.setOpaque(false);
         frame.add(this);
-        frame.setSize(1000, 800);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        color = col;
     }
 
-    int SIZE = 50;
-    int SPEED = 10;
+    public int SIZE = 50;
+    int SPEED = 1;
 
     Random losowanie = new Random();
 
-    int x = losowanie.nextInt(800)+1;
-    int y = losowanie.nextInt(800)+1;
+    public int x = losowanie.nextInt(800) + 100;
+    public int y = losowanie.nextInt(600) + 100;
     //int x =0, y =0;
     int angleX = SPEED, angleY = SPEED;
 
-    private void move()
-    {
-        if (x+angleX < 0)
+    private void move() {
+        if (x + angleX < 0)
             angleX = SPEED;
-        else if (x + angleX > getWidth() - SIZE)
+        else if (x + angleX > 987.5 - SIZE)
             angleX = -SPEED;
         else if (y + angleY < 0)
             angleY = SPEED;
-        else if (y+angleY>getHeight() - SIZE)
+        else if (y + angleY > 770 - SIZE)
             angleY = -SPEED;
 
         x = x + angleX;
         y = y + angleY;
-    }
 
-    // rysowanie pilki
-    @Override
-    public void paint(Graphics g)
-    {
-        // rozmiar pilki
-        super.paint(g);
-        g.fillOval(x,y,SIZE,SIZE);
+        //System.out.println(color.toString() + " ");
+        //System.out.print(x + " ");
+        //System.out.print(y + " ");
     }
 
     @Override
     public void run() {
         while (true) {
             this.move();
-            this.repaint();
+
             try {
-                Thread.sleep(10);
+                Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public static void main(String[] args) throws InterruptedException{
+    Color color;
+
+    @Override
+    public void paint(Graphics g) {
+        // rozmiar pilki
+        super.paint(g);
+
+        g.setColor(color);
+        g.fillOval(x, y, SIZE, SIZE);
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
         JFrame frame = new JFrame("Balls");
+        //JPanel panel = new Panel;
+        frame.setSize(1000, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        new Thread(new Main(frame)).start();
-        System.out.println("thread");
+        //Thread displ = new Thread(DisBall(frame));
+        //displ.start();
 
-        new Thread(new Main(frame)).start();
-        System.out.println("thread2");
+        Thread ball1 = new Thread(new Main(frame, Color.red));
+        ball1.start();
+
+        Thread ball2 = new Thread(new Main(frame, Color.blue));
+        ball2.start();
+
+        Thread ball3 = new Thread(new Main(frame, Color.green));
+        ball3.start();
+
+        Thread ball4 = new Thread(new Main(frame, Color.CYAN));
+        ball4.start();
+
+        Thread ball5 = new Thread(new Main(frame, Color.yellow));
+        ball5.start();
+
+        Thread ball6 = new Thread(new Main(frame, Color.magenta));
+        ball6.start();
+
+        while(true)
+        {
+            frame.repaint();
+            Thread.sleep(1);
+        }
+
     }
 }
